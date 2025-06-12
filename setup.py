f76ac2f54e5dfb1e4c4cd396529293425de79b4b
Chengshu Li <chengshu@stanford.edu>
2025-06-11 17:22:27 -0700

    add setup.py and scene instances

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='momagen',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'torchvision',
+        'cvxpy',
+        'open3d',
+        'wandb',
+        'einops',
+        'fpsample',
+        'gym'
+    ],
+    author='Chengshu Li',
+    description='MoMaGen: Generating Demonstrations under Soft and Hard Constraints for Multi-Step Bimanual Mobile Manipulation',
+    python_requires='>=3.10',
+)
+
